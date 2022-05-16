f99b017054182df5e9062d91aeb89b19bbe5bba0
prado <pradodje@gmail.com>
2022-05-15 17:42:50 -0700

    Add logic for user button presses
    - Added button numbers to elements
    - When user pushes a button, button number is added to polo list,
      once user pushes the required quntity of buttons, the sequence is
      sent to the server to be verified

--- static/script.js
+++ static/script.js
@@ -4,7 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const newGame = document.querySelector('#new-game')
     const win = document.querySelector('#win')
     let gameId = ''
-    let sequence = []
+    let marco = []
+    let polo = []
 
     // flashes the given button for 1/2 second
     const flashButton = (button) => {
@@ -19,28 +20,43 @@ document.addEventListener('DOMContentLoaded', () => {
     // sequence and the win status
     const fetchGame = async (id) => {
         const cs = await fetch(`/game/${id}`).then(r => r.json())
-        sequence.push(cs.currentSequence[cs.currentSequence.length - 1])
-        sequence.forEach(button => flashButton(button))
-        console.log(`Sequence: ${sequence}`)
+        marco.push(cs.currentSequence[marco.length])
+        marco.forEach(button => flashButton(button))
+        console.log(cs)
     }
 
     // starts a new game when the user clicks the new game button
     // flashes the first button of a newely generated button sequence
     newGame.addEventListener('click', async () => {
         gameId = await fetch('/game', {method: 'POST'}).then(r => r.text())
-        console.log(`New game id# ${gameId}`)
+        console.log(`New gameId: ${gameId}`)
         
         buttonList.forEach(button => button.setAttribute('disabled', 'false'))
-        sequence = []
+        marco = []
+        polo = []
         setTimeout(() => fetchGame(gameId), 1000)
     })
 
     // 
     buttonContainer.addEventListener('click', async (e) => {
-        if (e.target && e.target.nodeName == 'BUTTON') {
-            // record press
-        }
-        
-    })
+        if (e.target && e.target.dataset.num >= 0) {
+            polo.push(Number(e.target.dataset.num))
 
+            if (marco.length === polo.length) {
+                const correct = await fetch(`/game/${gameId}`, {
+                    method: 'POST',
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify(polo)
+                })
+                .then(r => r.text())
+                .then(r => r === 'true')
+                
+                if (correct) {
+                    console.log('right')
+                } else {
+                    console.log('wrong')
+                }
+            }
+        }
+    })
 })
