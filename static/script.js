4283f5c4f0681f768481faa3cf94336df8fd9be6
prado <pradodje@gmail.com>
2022-05-16 02:35:44 -0700

    Redo js from scratch
    - still need to fix real-time button highlight when pressing
    - need to review disabled button logic, might not be behaving as
      expected, can still record presses when it's disabled

--- static/script.js
+++ static/script.js
@@ -1,85 +1,132 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const buttonContainer = document.querySelector('#button-container')
     const buttonList = document.querySelectorAll('.colored-button')
-    const newGame = document.querySelector('#new-game')
-    const win = document.querySelector('#win')
-    let gameWon = false
-    let gameId = ''
+    const winMsg = document.querySelector('#win')
+    let hasWon = false
     let marco = []
     let polo = []
+    let id = ''
 
-    // flashes the given buttons for the specified count & delay
-    const flashButtons = async (buttons, count=1, delay=500) => {
-        return new Promise((accept, reject) => {
-            let i = 0
-            setInterval(() => {
-                if (i++ < count*2)
-                    buttons.forEach(b => buttonList[b].classList.toggle('flash'))
-                else
-                    accept()
-            }, delay)
+    // starts a new game when user clicks new game button
+    document.querySelector('#new-game').addEventListener('click', async () => {
+        buttonList.forEach(b => {
+            b.setAttribute('disabled', 'false')
+            b.classList.add('inactive')
         })
-    }
+        winMsg.classList.add('hidden')
 
-    // plays the current button sequence
-    const playMarco = () => {
-        marco.forEach(b => flashButtons([b]))
-    }
+        id = await fetch('/game', {method: 'POST'}).then(r => r.text())
+        await fetchGame()
+        start()
+    })
 
-    // fetches the current state of given game id, including the current button
-    // sequence and the win status
-    const fetchGame = async (id) => {
+    // fetch game details from server
+    const fetchGame = async () => {
         return new Promise((accept, reject) => {
             (async () => {
                 const game = await fetch(`/game/${id}`).then(r => r.json())
-                marco.push(game.currentSequence[marco.length])
-                gameWon = game.hasWon
+                console.log(game)
+                hasWon = game.hasWon
+                marco = game.currentSequence
                 accept()
             })()
         })
     }
-    
-    // starts a new game when the user clicks the new game button
-    // flashes the first button of a newely generated button sequence
-    newGame.addEventListener('click', async () => {
-        gameId = await fetch('/game', {method: 'POST'}).then(r => r.text())
-        console.log(`New gameId: ${gameId}`)
-        
+
+    const toggleFlash = (buttons) => {
+        buttons.forEach(b => buttonList[b].classList.toggle('flash'))
+    }
+
+    // flashes the specified buttons for the given count and delay
+    const flashButton = async (buttons, count=1, delay=350) => {
+        return new Promise((accept, reject) => {
+            let i = 0
+            const flash = setInterval(() => {
+                toggleFlash(buttons)
+                if (++i === count*2) {
+                    clearInterval(flash)
+                    accept()
+                }
+            }, delay)
+        })
+    }
+
+    // play the current button sequence
+    // disable buttons during play
+    const playMarco = async () => {
+        buttonList.forEach(b => b.setAttribute('disabled', 'true'))
+
+        for (let i = 0; i < marco.length; i++)
+            await flashButton([marco[i]])
+
         buttonList.forEach(b => b.setAttribute('disabled', 'false'))
-        marco = []
+    }
+
+    // start/restart
+    const start = () => {
         polo = []
-        setTimeout(async () => {
-            await fetchGame(gameId)
-            playMarco()
-        }, 1000)
+        playMarco()
+    }
+
+    document.querySelector('#button-container').addEventListener('mousedown', (e) => {
+        if (e.target && e.target.dataset.num >= 0)
+            e.target.classList.toggle('inactive')
     })
 
-    // button press logic
-    // if wrong button is pressed at any time, return from fuction
-    buttonContainer.addEventListener('click', async (e) => {
+    document.querySelector('#button-container').addEventListener('mouseup', (e) => {
+        if (e.target && e.target.dataset.num >= 0)
+            e.target.classList.toggle('inactive')
+    })
+
+    // handle button presses
+    document.querySelector('#button-container').addEventListener('click', (e) => {
         if (e.target && e.target.dataset.num >= 0) {
             polo.push(Number(e.target.dataset.num))
+            const n = polo.length
 
-            if (marco[polo.length-1] !== polo[polo.length-1]) {
-                // wrong button pressed
-                // flash all buttons and replay marco
-                await flashButtons([...Array(buttonList.length).keys()], 3, 200)
-                playMarco()
-            
-            } else if (marco.length === polo.length) {
-                const correct = await fetch(`/game/${gameId}`, {
-                    method: 'POST',
-                    headers: {'Content-Type': 'application/json'},
-                    body: JSON.stringify(polo)
-                })
-                .then(r => r.text())
-                .then(r => r === 'true')
-                
-                if (correct) {
-                    console.log('right')
-                } else {
-                    console.log('wrong')
-                }
+            if (marco[n-1] !== polo[n-1]) {
+                (async () => {
+                    await flashButton([...Array(buttonList.length).keys()], 3, 150)
+                    start()
+                })()
+
+            } else if (n === marco.length) {
+                // verify polo on server
+                // if correct, fetch new game details
+                // otherwise, restart
+                (async () => {
+                    const correct = await fetch(`/game/${id}`, {
+                        method: 'POST',
+                        headers: {'Content-Type': 'application/json'},
+                        body: JSON.stringify(polo)
+                    })
+                    .then(r => r.text())
+                    .then(r => r === 'true')
+                    
+                    if (correct) {
+                        // fetch new game details
+                        (async () => {
+                            await fetchGame()
+
+                            if (hasWon) {
+                                // end game
+                                buttonList.forEach(b => {
+                                    b.setAttribute('disabled', 'true')
+                                    b.classList.remove('inactive')
+                                })
+                                winMsg.classList.remove('hidden')
+                            
+                            } else
+                                // round not over, continue
+                                start()
+                        })()
+                        
+                    } else {
+                        // mismatch between server and marco
+                        // error if reach here
+                        console.log('Server-marco mismatch?')
+                        start()
+                    }
+                })()
             }
         }
     })
